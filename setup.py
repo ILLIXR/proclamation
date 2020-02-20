db22967d7044221019607fb1e4a6961390346eba
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-02-20 11:38:46 -0600

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+"""Setup."""
+
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
