89f010b3ce55d76938150d250840efe2f242aaa9
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-05-29 16:06:46 -0500

    Add a section setting test.

--- proclamation/test/test_settings.py
+++ proclamation/test/test_settings.py
@@ -6,7 +6,7 @@
 import json
 from io import StringIO
 
-from ..settings import parse_project, settings_from_json_io
+from ..settings import parse_project, settings_from_json_io, parse_section
 
 PROJ_NAME = "my project"
 
@@ -54,3 +54,13 @@ def test_project_toplevel():
     assert(settings.projects[0].name == PROJ_NAME)
     assert(settings.projects[0].template == "dummy")
     assert(len(settings.projects[0].sections) == 1)
+
+
+def test_parse_section():
+    sect_setting_dict = {
+        "directory": "changes/main"
+    }
+
+    sect = parse_section("main section", sect_setting_dict)
+    assert(sect.name == "main section")
+    assert(sect.directory == sect_setting_dict['directory'])
